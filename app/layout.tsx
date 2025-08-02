1a637041214fe184206caab42f7300b3c5ed7aca
F8TDDEV <sussystickispro@gmail.com>
2025-08-02 17:57:20 +1200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+	<head>
+	<meta name="apple-mobile-web-app-title" content="RDEV" />
+	</head>
       <body
         className="font-sans antialiased"
         style={{
